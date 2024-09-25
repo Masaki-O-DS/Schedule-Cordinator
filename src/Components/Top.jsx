b6a7650b57e778bab25e0eab7dcfe05ad949c529
Masaki Ogawa <pataporasu@gmail.com>
2024-09-25 22:54:58 +0900

    Topコンポーネントの設計とCSSで外観を整えた

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Top.css";
 
 const Top = () => {
-  return <div>Top</div>;
+  return (
+    <>
+      <div className="all">
+        <div className="title">
+          <h1 className="schedule">Schedule</h1>
+          <h1 className="coordinator">Coordinator</h1>
+        </div>
+        <div className="login">
+          <p>予定の調整を行うためにログインしてください。</p>
+          <button>Googleでログインする</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Top;
