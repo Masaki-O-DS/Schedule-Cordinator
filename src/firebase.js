86141c75e62168d910dce7081a4fb20e102d8346
Masaki Ogawa <pataporasu@gmail.com>
2024-09-25 17:24:01 +0900

    プロジェクトの作成

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ejJ66Czt6V32QdcehVwabHtPpSM93PY",
+  authDomain: "schedulecoordination-538cb.firebaseapp.com",
+  projectId: "schedulecoordination-538cb",
+  storageBucket: "schedulecoordination-538cb.appspot.com",
+  messagingSenderId: "657428092252",
+  appId: "1:657428092252:web:b65c242edd6a133c5e1d99",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
