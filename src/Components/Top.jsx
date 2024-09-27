13b0db1b65c3279c89ea7d25b724109caaed8fc7
Masaki Ogawa <pataporasu@gmail.com>
2024-09-27 16:30:20 +0900

    NewAdjustScheduleComponentsの作成完了

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const Top = ({ isAuth, setIsAuth }) => {
   const navigate = useNavigate();
+
   const [name, setName] = useState("");
 
   //googleでログイン
